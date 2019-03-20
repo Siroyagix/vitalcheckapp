ed62a0623dedd491f2d62c7ccd427258147265fa
Siroyagix <holymcontroler@gmail.com>
2019-03-20 23:08:20 +0900

    トップページのタイトル変更

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,15 @@
 |
 */
 
+/*
 Route::get('/welcome', function () {
     return view('welcome');
 });
+*/
 
 Auth::routes();
 
-/*Route::get('/home', 'HomeController@index')->name('home');*/
+/*
+Route::get('/home', 'HomeController@index')->name('home');
+*/
 Route::get('/','HomeController@index');
