66f19a90f0a79536f5b7015d9d0d5a8324ce73b4
Siroyagix <holymcontroler@gmail.com>
2019-03-22 20:26:59 +0900

    新規登録画面の編集

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,5 @@ Auth::routes();
 /*
 Route::get('/home', 'HomeController@index')->name('home');
 */
-Route::get('/','HomeController@index');
+Route::get('/','HomeController@home');
+Route::get('home','HomeController@index');
