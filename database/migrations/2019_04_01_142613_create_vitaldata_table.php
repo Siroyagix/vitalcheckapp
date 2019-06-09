44977ea9ede7a6c78bfad1327db0a666c3f309d9
Siroyagix <holymcontroler@gmail.com>
2019-06-09 15:49:09 +0900

    マイグレーションファイルを修正。nullableを追加

--- database/migrations/2019_04_01_142613_create_vitaldata_table.php
+++ database/migrations/2019_04_01_142613_create_vitaldata_table.php
@@ -17,13 +17,13 @@ class CreateVitaldataTable extends Migration
             $table->bigIncrements('id');
             $table->integer('user_id');
             $table->date('date')->comment('日付');
-            $table->decimal('bodytemperature',3,1)->comment('体温');
-            $table->integer('pulse')->comment('脈拍');
-            $table->integer('systolicbp')->comment('収縮期血圧');
-            $table->integer('diastlicbp')->comment('拡張期血圧');
-            $table->text('excretion')->comment('排泄量');
-            $table->text('stoolform')->comment('便の性状');
-            $table->text('freecomments')->comment('フリーコメント');
+            $table->decimal('bodytemperature',3,1)->nullable()->comment('体温');
+            $table->integer('pulse')->nullable()->comment('脈拍');
+            $table->integer('systolicbp')->nullable()->comment('収縮期血圧');
+            $table->integer('diastlicbp')->nullable()->comment('拡張期血圧');
+            $table->text('excretion')->nullable()->comment('排泄量');
+            $table->text('stoolform')->nullable()->comment('便の性状');
+            $table->text('freecomments')->nullable()->comment('フリーコメント');
             $table->timestamps();
         });
     }
